7dd7d31eafd36a2af02685756146d498f288d640
Christian Schüler <sc.schueler@gmail.com>
2026-03-10 23:15:07 +0100

    update sorting algorithm and added useState

--- src/Table.tsx
+++ src/Table.tsx
@@ -4,12 +4,44 @@ import data from './api-result.json';
 import Thead from './Thead';
 import Tbody from './Tbody';
 import Tfoot from './Tfoot';
+import { useState } from 'react';
 
 const Table = () => {
-  const filteredSortedData = data;
+  const [filteredSortedData, setFilteredSortedData] = useState(data);
 
-  const sortUpDown = (key: string, direction: string) => {
-    console.log(key, direction);
+  const sortUpDown = (
+    key: 'category' | 'name' | 'price' | 'stocked',
+    direction: string,
+  ) => {
+    setFilteredSortedData(
+      filteredSortedData.sort(
+        (
+          a: {
+            category: string;
+            name: string;
+            price: number;
+            stocked: boolean;
+          },
+          b: {
+            category: string;
+            name: string;
+            price: number;
+            stocked: boolean;
+          },
+        ) => {
+          const valueA = (a[key] as string).toUpperCase();
+          const valueB = (b[key] as string).toUpperCase();
+
+          // returning different values depending on the sort direction
+          if (valueA < valueB) {
+            return direction === 'up' ? -1 : 1;
+          } else if (valueA > valueB) {
+            return direction === 'up' ? 1 : -1;
+          }
+          return 0;
+        },
+      ),
+    );
   };
 
   return (
--- src/Tbody.tsx
+++ src/Tbody.tsx
@@ -1,7 +1,39 @@
 import './Tbody.css';
 
-const Tbody = ({ data }: { data: any[] }) => {
-  return <tbody className="tbody"></tbody>;
+const Tbody = ({
+  data,
+}: {
+  data: {
+    category: string;
+    name: string;
+    price: number;
+    stocked: boolean;
+  }[];
+}) => {
+  return (
+    <tbody className="tbody">
+      {data.map(
+        (
+          row: {
+            category: string;
+            name: string;
+            price: number;
+            stocked: boolean;
+          },
+          key,
+        ): any => {
+          return (
+            <tr key={key}>
+              <td>{row.category}</td>
+              <td>{row.name}</td>
+              <td>{row.price.toLocaleString()}</td>
+              <td>{row.stocked ? 'vorhanden' : 'nicht vorhanden'}</td>
+            </tr>
+          );
+        },
+      )}
+    </tbody>
+  );
 };
 
 export default Tbody;
